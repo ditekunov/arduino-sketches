f520d46e5dbc8ca113efd3ccacc56db971bd1bc5
Tekunov Daniil <ditekunov@yandex.ru>
2019-01-27 10:17:05 +0300

    Update temp_light.ino

--- iot/temp_light.ino
+++ iot/temp_light.ino
@@ -1,1 +1,38 @@
+#include <SPI.h>
+#include <SD.h>
+#include <math.h>
 
+
+#define LIGHT_PIN A3
+#define TEMP_PIN  A2
+#define CS        8
+
+float r_light, light, v_temp, temp;
+String data;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!SD.begin(CS)) {
+    Serial.println("Cannot initialize SD port");
+    return;
+  }
+}
+
+void loop() {
+  r_light = 10.0 / (1023.0 / analogRead(LIGHT_PIN) - 1.0);
+  light = 10.0 * pow(14.0 / r_light, 1.6);
+  v_temp = 1023.0 / analogRead(TEMP_PIN) - 1.0;
+  temp = 1.0 / (-log(v_temp) / 3977.0 + 1.0 / 295.0) - 273.0;
+
+  data = String(millis() / 1000) + ";" + String(light) + ";" + String(temp);
+
+  data.replace(".", ",");
+  Serial.println(data);
+
+  File logF = SD.open("log.csv", FILE_WRITE);
+  logF.println(data);
+  logF.close();
+
+  delay(1000);
+}
